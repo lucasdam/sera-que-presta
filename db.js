6fae9c1065d1687ed026621584b5f1e0012f4fcd
Lucas Damasceno <54273070+lucasdam@users.noreply.github.com>
2020-04-27 18:25:22 -0300

    Add files via upload

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
 const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/seraquepresta'
 module.exports = mongoose.connect(url, { useMongoClient: true })
 
-const config = require('../knexfile.js')
-const knex = require('knex') (config)
+//const config = require('../knexfile.js')
+//const knex = require('knex') (config)
 
 //Exportando o knex para o index
 knex.migrate.latest([config])
